d4b446b767e6d8be750c0d1a90d95d5ba9c3bc79
EC2 Default User <ec2-user@ip-172-31-50-109.ec2.internal>
2021-10-15 20:30:03 +0000

    assignemnt 5.2

--- Week-7-processBlog/processBlog.js
+++ Week-7-processBlog/processBlog.js
@@ -1,9 +1,11 @@
+//create items to insert into table per the process blog
+
 var blogEntries = [];
 
 class BlogEntry {
-  constructor(bread, date, flour, salt, water, yeast, unitOfMeasurement, folds, foldCount, totalTime, result) {
+  constructor(bread, date, flour, salt, water, yeast, unitOfMeasurement, folds, foldCount, totalTimeHours, totalTimeMinutes, result) {
     this.bread = {};
-    this.bread.N = bread.toString();
+    this.bread.S = bread;
     this.date = {}; 
     this.date.S = new Date(date).toDateString();
     this.flour = {};
@@ -21,8 +23,10 @@ class BlogEntry {
     if (foldCount != null) {
       this.foldCount = {};
       this.foldCount.N = foldCount.toString(); 
-    this.totalTime = {};
-    this.totalTime.S = totalTime;
+    this.totalTimeHours = {};
+    this.totalTimeHours.N = totalTimeHours.toString();
+    this.totalTimeMinutes = {};
+    this.totalTimeMinutes.N = totalTimeMinutes.toString();
     this.result = {};
     this.result.S = result;
     }
@@ -31,28 +35,33 @@ class BlogEntry {
   }
 }
 
-blogEntries.push(new BlogEntry('50% Whole Wheat Bread with Biga', 'June 25, 2020', 500, 22, 400, 3, 'grams', true, 3, '19 hours 45 minutes', 'Crisp. good rise due to banneton basket. Hearty and dense.'));
-blogEntries.push(new BlogEntry('Harvest Bread with Poolish', 'May 29, 2020', 500, 2, 390, 1.7, 'grams', true, 2, '16 hours 15 minutes', 'Decent, large bubbles. Good height and crisp crust. Like a healthy wheat bread with the lightness of white bread.'));
-blogEntries.push(new BlogEntry('Overnight White Bread with Poolish', 'May 13, 2020', 500, 10.5, 375, 1.7, 'grams', true, 3, '16 hours 15 minutes', 'Softer than normal, fendue shape of loaf was lost in baking, otherwise normal rise.'));
+blogEntries.push(new BlogEntry('50% Whole Wheat Bread with Biga', 'June 25 2020', 500, 22, 400, 3, 'grams', true, 3, 19, 45, 'Crisp. good rise due to banneton basket. Hearty and dense.'));
+blogEntries.push(new BlogEntry('Harvest Bread with Poolish', 'May 29 2020', 500, 2, 390, 1.7, 'grams', true, 2, 16, 15, 'Decent, large bubbles. Good height and crisp crust. Like a healthy wheat bread with the lightness of white bread.'));
+blogEntries.push(new BlogEntry('Overnight White Bread with Poolish', 'May 13 2020', 500, 10.5, 375, 1.7, 'grams', true, 3, 16, 15, 'Softer than normal, fendue shape of loaf was lost in baking, otherwise normal rise.'));
 
 
 console.log(blogEntries);
 
 
-////// starter code 
+////// starter code - populate your 'table'
 
 
-// var AWS = require('aws-sdk');
-// AWS.config = new AWS.Config();
-// AWS.config.region = "us-east-1";
+var AWS = require('aws-sdk');
+AWS.config = new AWS.Config();
+AWS.config.region = "us-east-1";
+async = require('async')
 
-// var dynamodb = new AWS.DynamoDB();
+var dynamodb = new AWS.DynamoDB();
 
-// var params = {};
-// params.Item = blogEntries[0]; 
-// params.TableName = "processBlog";
+async.eachSeries(blogEntries, function(value, callback) {
 
-// dynamodb.putItem(params, function (err, data) {
-//   if (err) console.log(err, err.stack); // an error occurred
-//   else     console.log(data);           // successful response
-// });
+var params = {};
+params.Item = value; 
+params.TableName = "processBlog";
+
+dynamodb.putItem(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
+    setTimeout(callback, 1000); 
+});
